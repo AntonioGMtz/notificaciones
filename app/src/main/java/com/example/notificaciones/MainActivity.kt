7833a49ccb0fb0c59b4e3edc30d05a307fdcf823
Antonio <antoniogallegos086@gmail.com>
2023-05-25 11:07:55 -0600

    Adding main interface

--- app/src/main/java/com/example/notificaciones/MainActivity.kt
+++ app/src/main/java/com/example/notificaciones/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.notificaciones
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val MY_CHANNEL_ID = "myChannel"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun createSimpleNotification()
+    {
+
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val flag = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, flag)
+
+        var builder = NotificationCompat.Builder(this, MY_CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.ic_delete)
+            .setContentTitle("My title")
+            .setContentText("Esto es un ejemplo <3")
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText("Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi Holita holi ")
+            )
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
     }
 }
